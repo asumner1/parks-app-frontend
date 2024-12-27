9d97631e66a7e46bbe59f9a44542775d41fa331f
asumner1 <andrew.r.sumner@gmail.com>
2024-12-27 13:47:51 -0500

    another fix attempt

--- app/park/[id]/page.tsx
+++ app/park/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getParkData } from '@/app/actions/getParkData';
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
 
+/* @typescript-eslint/no-explicit-any */
 export async function generateMetadata({ params }: any) {
   const parks = await getParkData();
   const park = parks.find(p => p.id === params.id);
@@ -18,6 +19,7 @@ export async function generateMetadata({ params }: any) {
   };
 }
 
+/* @typescript-eslint/no-explicit-any */
 export default async function Page({ params }: any) {
   const parks = await getParkData();
   const park = parks.find(p => p.id === params.id);
