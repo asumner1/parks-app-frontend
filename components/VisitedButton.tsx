752c91b3432570c9ad48656f25b3cf094dbf8880
asumner1 <andrew.r.sumner@gmail.com>
2025-01-17 21:57:19 -0500

    removed unused imports

--- components/VisitedButton.tsx
+++ components/VisitedButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import { FaCheck } from 'react-icons/fa';
 import { useAccountPitch } from '@/contexts/AccountPitchContext';
 import { useUser } from '@/hooks/useUser';
